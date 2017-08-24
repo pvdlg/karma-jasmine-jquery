ba9d8ec39558dd9fdfec34cfc08b4daf2f85c91b
Pierre-Denis Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-08-24 17:38:21 -0400

    feat(rollup): implement changes to support Rollup 0.48.x ✨

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,7 +2,10 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  format: 'es',
-  entry: 'src/jasmine-jquery.bundle.js',
+  input: 'src/jasmine-jquery.bundle.js',
+  output: {
+    file: 'lib/jasmine-jquery.bundle.js',
+    format: 'es',
+  },
   plugins: [nodeResolve(), commonjs({include: ['node_modules/**/*']})],
 };
