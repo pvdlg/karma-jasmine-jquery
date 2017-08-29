2a745d44961febf0cb972267e38d510e2ea18ee6
Pierre-Denis Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-08-29 14:06:29 -0400

    chore(eslint): Use `eslint-config-pretty`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,5 @@
 module.exports = {
-  extends: [
-    '@metahub/eslint-config/es6-config',
-    '@metahub/eslint-config/node-config',
-    '@metahub/eslint-config/promise-config',
-    '@metahub/eslint-config/jasmine-config',
-    '@metahub/eslint-config/ava-config',
-    '@metahub/eslint-config/prettier-config',
-  ],
+  extends: ['pretty/es6', 'pretty/node', 'pretty/promise', 'pretty/jasmine', 'pretty/ava', 'pretty/prettier'],
   env: {browser: true, jquery: true},
   parserOptions: {sourceType: 'module'},
 };
