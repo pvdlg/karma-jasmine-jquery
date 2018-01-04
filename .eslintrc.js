940336ccfe537e6a047ac188af46485f860eac12
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-01-04 00:47:37 -0500

    chore: use XO instead of custom eslint

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  extends: ['pretty/es6', 'pretty/node', 'pretty/promise', 'pretty/jasmine', 'pretty/ava', 'pretty/prettier'],
-  env: {browser: true, jquery: true},
-  parserOptions: {sourceType: 'module'},
-};
